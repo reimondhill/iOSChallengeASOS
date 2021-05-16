f9ab35d61f21c928d814482e7ba6f124019b138d
Ramon Haro Marques <ramonharomarques@gmail.com>
2021-05-16 10:33:41 +0100

    Added icons

---
+++ Data/Data/Data.h
@@ -0,0 +1,18 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Ramon Haro Marques on 16/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
---
+++ Presentation/Presentation/Presentation.h
@@ -0,0 +1,18 @@
+//
+//  Presentation.h
+//  Presentation
+//
+//  Created by Ramon Haro Marques on 16/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Presentation.
+FOUNDATION_EXPORT double PresentationVersionNumber;
+
+//! Project version string for Presentation.
+FOUNDATION_EXPORT const unsigned char PresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Presentation/PublicHeader.h>
+
+
